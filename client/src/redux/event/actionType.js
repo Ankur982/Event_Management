fea49e674405fd6a7630badde5eddb81b6ee6c44
Ankur982 <singh999ankur@gmail.com>
2023-01-10 23:44:01 +0530

    adding event reducer

---
+++ client/src/redux/event/actionType.js
@@ -0,0 +1,3 @@
+export const LOADING = "LOADING";
+export const ERROR = "ERROR";
+export const GET_EVENTS = "GET_EVENTS";
--- client/src/redux/event/reducer.js
+++ client/src/redux/event/reducer.js
@@ -1,12 +1,39 @@
+import { ERROR, GET_EVENTS, LOADING } from "./actionType"
 
 
 const initState = {
-    
+    loading: false,
+    error: false,
+    events: [],
 }
 
 export const eventReducer = (state = initState, { type, payload }) => {
     switch (type) {
+        case LOADING: {
+            return {
+                ...state,
+                loading: true,
+                error: false
+            }
+        }
 
+        case ERROR: {
+            return {
+                ...state,
+                loading: false,
+                error: true
+            }
+        }
+
+
+        case GET_EVENTS: {
+            return {
+                ...state,
+                loading: false,
+                error: false,
+                events: payload
+            }
+        }
         
 
         default: return state
